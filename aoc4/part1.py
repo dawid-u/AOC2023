30484c137c7ec41979a8fc3d0faa949955db82f8
dawid-u <89160244+dawid-u@users.noreply.github.com>
2023-12-08 08:18:22 +0100

    Add files via upload

---
+++ aoc4/part1.py
@@ -0,0 +1,18 @@
+with open('input.txt', 'r') as file:
+    cp = []
+    for line in file:
+        cp.append(line[8:].strip("\n").split("|"))
+    total = 0
+    for index, line in enumerate(cp):
+        copy = []
+        value = 0
+        for element in line:
+            copy.append(element.split(" "))
+        for number in copy[1]:
+            if number in copy[0] and number != "":
+                if value == 0:
+                    value += 1
+                else:
+                    value = value*2
+        total += value
+        print(total, line, index)
